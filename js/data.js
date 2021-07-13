a71000b29a145d899b7afbb6c090462d33742b21
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-13 14:40:58 -0700

    Moved code that does not handle local storage to main.js and just localStorage handling code in data.js

--- js/data.js
+++ js/data.js
@@ -6,27 +6,7 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
-var $img = document.getElementsByTagName('img')[0];
-var placeholder = $img.getAttribute('src');
-var $form = document.querySelector('form');
-var $elements = $form.elements;
-var $entryTitle = $elements.title;
-var $photoUrl = $elements.photoUrl;
-var $entryNotes = $elements.notes;
 
-$form.addEventListener('submit', handleSubmit);
-function handleSubmit(event) {
-  var $newEntry = {};
-  $newEntry[$entryTitle.name] = $entryTitle.value;
-  $newEntry[$photoUrl.name] = $photoUrl.value;
-  $newEntry[$entryNotes.name] = $entryNotes.value;
-  $newEntry.nextEntryId = data.nextEntryId;
-  data.nextEntryId += 1;
-  data.entries.unshift($newEntry);
-  $form.reset();
-  $img.setAttribute('src', placeholder);
-  event.preventDefault();
-}
 var previousEntryJSON = localStorage.getItem('javascript-local-storage');
 if (previousEntryJSON !== null) {
   data = JSON.parse(previousEntryJSON);

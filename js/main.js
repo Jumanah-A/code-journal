c866f566e9744733bf98343c3385d5aae7b77afc
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-13 11:53:21 -0700

    Handeled the updtaed image based on the user's image URL input, handled case if user deleted input the placeholder image takes place

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 /* global data */
 /* exported data */
+
+var $form = document.querySelector('form');
+var $elements = $form.elements;
+var $photoUrl = $elements.photoUrl;
+var $img = document.getElementsByTagName('img')[0];
+var placeholder = $img.getAttribute('src');
+
+$photoUrl.addEventListener('input', handlePhoto);
+function handlePhoto(event) {
+  if ($photoUrl.value === '') {
+    $img.setAttribute('src', placeholder);
+  } else {
+    $img.setAttribute('src', $photoUrl.value);
+  }
+}

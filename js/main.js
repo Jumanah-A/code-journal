faf19d987b80fe195d0faa63e6f2c6a6079e5649
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-13 14:39:53 -0700

    Moved the data handling code from data.js to main.js and fixed entryId variable name

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var $elements = $form.elements;
 var $photoUrl = $elements.photoUrl;
 var $img = document.getElementsByTagName('img')[0];
 var placeholder = $img.getAttribute('src');
+var $entryTitle = $elements.title;
+var $entryNotes = $elements.notes;
 
 $photoUrl.addEventListener('input', handlePhoto);
 function handlePhoto(event) {
@@ -14,4 +16,18 @@ function handlePhoto(event) {
   } else {
     $img.setAttribute('src', $photoUrl.value);
   }
+}
+
+$form.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  var $newEntry = {};
+  $newEntry[$entryTitle.name] = $entryTitle.value;
+  $newEntry[$photoUrl.name] = $photoUrl.value;
+  $newEntry[$entryNotes.name] = $entryNotes.value;
+  $newEntry.entryId = data.nextEntryId;
+  data.nextEntryId += 1;
+  data.entries.unshift($newEntry);
+  $form.reset();
+  $img.setAttribute('src', placeholder);
 }

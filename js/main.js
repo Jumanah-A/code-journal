f16b5668820dc606f4d232b89536f6787f1e7a85
Jumanah Almajnouni <jumanahh@outlook.com>
2021-07-14 15:07:41 -0700

    Added function forcreating the DOM tree and looping through the elements to add html code and added code for view swapping

--- js/main.js
+++ js/main.js
@@ -30,9 +30,60 @@ function handleSubmit(event) {
   data.entries.unshift($newEntry);
   $form.reset();
   $img.setAttribute('src', placeholder);
+  document.querySelector('.entries').className = 'entries';
+  document.querySelector('.entry-form').className = 'entry-form hidden';
+  document.location.reload();
+
 }
 
-// function entryTree(entry)
-// {
+function entryTree(entry) {
+  var notesText = document.createElement('p');
+  notesText.className = 'bottom-padding';
+  notesText.textContent = entry.notes;
+  var entryHeading = document.createElement('h1');
+  entryHeading.className = 'top-padding-one bottom-padding';
+  entryHeading.textContent = entry.title;
 
-// }
+  var entryImg = document.createElement('img');
+  entryImg.setAttribute('src', entry.photoUrl);
+
+  var halfText = document.createElement('div');
+  halfText.className = 'column-half bottom-padding';
+  halfText.appendChild(entryHeading);
+  halfText.appendChild(notesText);
+
+  var halfImg = document.createElement('div');
+  halfImg.className = 'column-half bottom-padding';
+  halfImg.appendChild(entryImg);
+
+  var block = document.createElement('div');
+  block.className = 'row entry';
+  block.appendChild(halfImg);
+  block.appendChild(halfText);
+  return block;
+}
+window.addEventListener('DOMContentLoaded', handleEntry);
+function handleEntry(event) {
+  var $ul = document.querySelector('ul');
+  for (var i = 0; i < data.entries.length; i++) {
+    $ul.appendChild(entryTree(data.entries[i]));
+  }
+}
+
+if (data.entries.length === 0) {
+  document.querySelector('.no-entries').className = 'no-entries';
+
+} else {
+  document.querySelector('.no-entries').className = 'no-entries hidden';
+}
+
+function handleClick(event) {
+  if (event.target.matches('.entries-tab')) {
+    document.querySelector('.entries').className = 'entries';
+  } else if (event.target.matches('.new-button')) {
+    document.querySelector('.entry-form').className = 'entry-form';
+    document.querySelector('.entries').className = 'entries hidden';
+  }
+}
+document.querySelector('header').addEventListener('click', handleClick);
+document.querySelector('.new-button').addEventListener('click', handleClick);
